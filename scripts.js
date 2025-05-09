1241df37afe5627264c451da11a6cf7363973392
nublius <184036331+nublius@users.noreply.github.com>
2025-05-08 17:52:37 -0700

    book constructor

--- scripts.js
+++ scripts.js
@@ -5,5 +5,9 @@ function Book() {
 };
 
 function addBookToLibrary() {
-
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readOrNot = readOrNot;
+    this.id = crypto.randomUUID();
 };
