fdac16dfa1c8453330d58a7550a8e1a39849d534
nublius <184036331+nublius@users.noreply.github.com>
2025-05-08 17:54:02 -0700

    book constructor error handling

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ function Book() {
 };
 
 function addBookToLibrary() {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor.");
+    };
     this.title = title;
     this.author = author;
     this.pages = pages;
