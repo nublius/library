266f10f1779af2696988ca5f753d1e0fbf73a18c
nublius <184036331+nublius@users.noreply.github.com>
2025-05-10 22:22:22 -0700

    change img svgs to inline svgs for easier styling

--- scripts.js
+++ scripts.js
@@ -66,11 +66,17 @@ function loadBook(book) {
 
     const newArticleDelete = document.createElement("button");
     newArticleDelete.className = "article__delete";
-    const deleteIcon = document.createElement("img");
-    deleteIcon.className = "icon";
-    deleteIcon.src = "images/delete.svg";
-    newArticleDelete.append(deleteIcon);
+
+    // Inline SVG as a string
+    const svgIcon = `
+    <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z"/></svg>
+    `;
+
+    // Add the SVG to the button
+    newArticleDelete.insertAdjacentHTML("beforeend", svgIcon);
+
     newArticleOptions.append(newArticleDelete);
+
 
     newArticleDelete.addEventListener("click", () => {
         const index = findBook(book.id);
